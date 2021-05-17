00eb216922d25e315f3430f09cc0f0256d5d6319
isaladkou <ivansaladkou.work@gmail.com>
2021-05-17 22:57:53 +0300

    Начата работа с фабричным методом

---
+++ patternsapp/src/main/java/by/artezio/saladkou/generating/factorymethod/Tank.java
@@ -0,0 +1,8 @@
+package by.artezio.saladkou.generating.factorymethod;
+
+public interface Tank {
+
+    int shoot();
+
+    void move(int x, int y);
+}
---
+++ patternsapp/src/main/java/by/artezio/saladkou/generating/factorymethod/Tiger.java
@@ -0,0 +1,14 @@
+package by.artezio.saladkou.generating.factorymethod;
+
+public class Tiger implements Tank {
+
+    @Override
+    public int shoot() {
+        return 0;
+    }
+
+    @Override
+    public void move(final int x, final int y) {
+
+    }
+}
